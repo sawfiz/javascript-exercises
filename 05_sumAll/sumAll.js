11182b556eac6e8ce4ae4c38af2c6a7b23e742d9
sawfiz <dashu.up@gmail.com>
2022-09-12 15:24:26 +0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    // Check if negative numbers
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    // Check if non-numbers
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
+
+    // Start should be smaller than end
+    start = a < b ? a : b;
+    end = a < b ? b : a;
+
+    // Calculate the sum.
+    let sum = 0;
+    for (num = start; num <= end; num++) {
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
