8ddf40d84eb07e1775fa34813b26585ddc9b9aa2
sawfiz <dashu.up@gmail.com>
2022-09-12 17:56:57 +0800

    Update 06_leapYears with simpler logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,14 @@
-const leapYears = function(year) {
-    if ((year % 4) !== 0) {
-        return false;
-    }
-    if (((year % 100) === 0) && ((year % 400) !== 0)) {
-        return false;
-    }
-    return true;
+const leapYears = function (year) {
+    // if (year % 4 !== 0) {
+    //     return false;
+    // }
+    // if (year % 100 === 0 && year % 400 !== 0) {
+    //     return false;
+    // }
+    // return true;
+
+    // Use logic A and (not B) + C
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
 
 // Do not edit below this line
