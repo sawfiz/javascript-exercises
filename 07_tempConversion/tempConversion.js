c8c360661c52428f3c2654b189da97a9bfe5a8d4
sawfiz <dashu.up@gmail.com>
2022-09-12 18:01:53 +0800

    Update 07_tempConversion with simpler logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,25 +1,31 @@
-const ftoc = function(degreeF) {
-  let degreeC = (degreeF -32) * 5 / 9;
-  if (Number.isInteger(degreeC)) {
-    return degreeC;
-  }
-  else {
-    return +degreeC.toFixed(1);
-  }
+const ftoc = function (degreeF) {
+    // let degreeC = (degreeF -32) * 5 / 9;
+    // if (Number.isInteger(degreeC)) {
+    //   return degreeC;
+    // }
+    // else {
+    //   return +degreeC.toFixed(1);
+    // }
+
+    // Use round(number * 10) /10 to achieve 1 decimal, and no decimal if integer
+    return Math.round((((degreeF - 32) * 5) / 9) * 10) / 10;
 };
 
-const ctof = function(degreeC) {
-  let degreeF = degreeC * 9 / 5 + 32;
-  if (Number.isInteger(degreeF)) {
-    return degreeF;
-  }
-  else {
-    return +degreeF.toFixed(1);
-  }
+const ctof = function (degreeC) {
+    // let degreeF = degreeC * 9 / 5 + 32;
+    // if (Number.isInteger(degreeF)) {
+    //   return degreeF;
+    // }
+    // else {
+    //   return +degreeF.toFixed(1);
+    // }
+
+    // Use round(number * 10) /10 to achieve 1 decimal, and no decimal if integer
+    return Math.round((degreeC * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
