d431701cd7edc5ab9274bec1c05e4a547fc65a8a
sawfiz <dashu.up@gmail.com>
2022-09-12 15:50:01 +0800

    Complete 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(degreeF) {
+  let degreeC = (degreeF -32) * 5 / 9;
+  if (Number.isInteger(degreeC)) {
+    return degreeC;
+  }
+  else {
+    return +degreeC.toFixed(1);
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(degreeC) {
+  let degreeF = degreeC * 9 / 5 + 32;
+  if (Number.isInteger(degreeF)) {
+    return degreeF;
+  }
+  else {
+    return +degreeF.toFixed(1);
+  }
 };
 
 // Do not edit below this line
