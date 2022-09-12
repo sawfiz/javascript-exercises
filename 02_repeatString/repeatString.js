70c25cf0f0719f416a43409143335d1e7fbf45c2
sawfiz <dashu.up@gmail.com>
2022-09-12 14:21:12 +0800

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let output = "";
+    for (let index = 0; index < num; index++) {
+       output = output + string; 
+    }
+    return output;
 };
 
 // Do not edit below this line
