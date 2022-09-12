cee9232101df236b1c7cbe95323a0cb223c25d14
sawfiz <dashu.up@gmail.com>
2022-09-12 14:34:03 +0800

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let len = string.length;
+    let newString = "";
+    for (i=len; i>=0; i--) {
+        newString = newString + string.charAt(i);
+    }
+    return newString;
 };
+
+// reverseString("Hello")
 
 // Do not edit below this line
 module.exports = reverseString;
