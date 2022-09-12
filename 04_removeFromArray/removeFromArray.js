a838b3ad32be1de0db6c65b40609c6c362c46572
sawfiz <dashu.up@gmail.com>
2022-09-12 15:06:59 +0800

    Compete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    // arguments[] is an array of arguments this function receives.
+    // arguments[0] is the array where items may need to be removed.
+    // starting from arguments[1] are the items that need to be removed from the array.
 
+    // The for loops starts from 1 as arguments[0] is the array that's passed in.
+    for (let i = 1; i < arguments.length; i++) {
+        for (let index = 0; index < arguments[0].length; index++) {
+            if (arguments[0][index] === arguments[i]) {
+                arguments[0].splice(index, 1)
+            } 
+        }   
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
