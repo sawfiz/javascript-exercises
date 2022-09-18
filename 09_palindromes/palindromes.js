b9fdfd50fe00c0aecc619b7525b97881c8f94292
sawfiz <dashu.up@gmail.com>
2022-09-18 22:40:31 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanedString = string.replace(/[^A-Za-z]/g, "").toLowerCase();
+    // console.log(cleanedString);
+    const reversedString = reverseString(cleanedString);
+    return reversedString === cleanedString ? true : false;
 };
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+// console.log(palindromes("Racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
