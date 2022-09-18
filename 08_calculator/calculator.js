0896d8799b42c7b70ec6f9a571fccd617e13a2ba
sawfiz <dashu.up@gmail.com>
2022-09-18 22:13:38 +0800

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    return array.reduce(getSum, 0);
 };
 
-const multiply = function() {
+function getSum(total, num) {
+    return total + num;
+}
 
+const multiply = function (array) {
+    return array.reduce(getProduct, 1);
 };
 
-const power = function() {
-	
+function getProduct(product, num) {
+    return product * num;
+}
+
+const power = function (base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return factorial(n - 1) * n;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
