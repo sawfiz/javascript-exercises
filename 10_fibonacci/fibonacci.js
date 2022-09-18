57e69491147bc45481b552c8a8b7040d47084486
sawfiz <dashu.up@gmail.com>
2022-09-18 22:52:07 +0800

    Compte 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (+n < 1) {
+        return "OOPS"
+    } else if (+n === 1 || +n === 2) {
+        return 1;
+    } else {
+        return fibonacci(+n - 1) + fibonacci(+n - 2);
+    }
 };
 
 // Do not edit below this line
